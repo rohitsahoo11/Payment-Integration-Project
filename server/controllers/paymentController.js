159ff3849b4a66875dde8cd2bf2cdf9a57427d89
Rohit Sahoo <rohitsahoo112003@gmail.com>
2024-10-03 17:03:10 +0530

    File Setup

---
+++ server/controllers/paymentController.js
@@ -0,0 +1,15 @@
+import { instance } from "../server.js"
+
+export const checkout = async(req,res)=>{
+
+    const options = {
+        amount: 50000, // amount in the smallest currence unit
+        currency: "INR",
+    }
+    const order = await instance.orders.create(options)
+    console.log(order)
+    res.status(200).json({
+        sucess:true,
+    })
+}
+
